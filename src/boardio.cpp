32874f6fd3270c67a39d81f8d337356e4c988e1e
Kevin Frei <freik@fb.com>
2018-09-22 20:45:29 -0700

    Hopefully address the right, 4th row issues...

--- src/boardio.cpp
+++ src/boardio.cpp
@@ -30,6 +30,7 @@ uint64_t BoardIO::Read() const {
   for (uint64_t colNum = 0; colNum < numcols; ++colNum) {
     uint64_t val = 1ULL << colNum;
     digitalWrite(cols[colNum], LOW);
+    delay(1);
     for (uint64_t rowNum = 0; rowNum < numrows; ++rowNum) {
       if (!digitalRead(rows[rowNum])) {
         switches |= val << (rowNum * numcols);
