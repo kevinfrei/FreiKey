e406349b23fcc240189fa581b62971eb297fdaa1
Kevin Frei <freik@fb.com>
2019-09-13 23:02:05 -0700

    Added compile_commands building for a clean build

--- src/clean-build.sh
+++ src/clean-build.sh
@@ -4,5 +4,10 @@ make -f left-btuart.mak -j allclean
 make -f right-btuart.mak -j allclean
 make -f betterfly.mak -j allclean
 ./build.sh
+make -f master.mak compile_commands
+make -f left-btuart.mak compile_commands
+make -f right-btuart.mak compile_commands
+make -f betterfly.mak compile_commands
+
 
 
