50b237238deea492ea8f4853094095016e172d5a
Kevin Frei <kevinfrei@hotmail.com>
2020-06-14 12:48:27 -0700

    Karbon build helper

---
+++ src/k-build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo Karbon-Master
+make -f karbon.mak -j all compile_commands
+echo Karbon-Left
+make -f l-karbon.mak -j all compile_commands
+echo Karbon-Right
+make -f r-karbon.mak -j all compile_commands
