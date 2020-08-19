afe2485520a58af3e9eec0870a895417172d4da0
Kevin Frei <kevinfrei@hotmail.com>
2020-08-19 08:21:33 -0700

    Ready for prime time, hopefully.

--- src/include/kbclient.h
+++ src/include/kbclient.h
@@ -10,7 +10,7 @@ class KBClient {
   static uint32_t stateTime;
   static state::hw lastRead;
   static uint16_t noChanges;
-  static constexpr uint16_t CHANGE_COUNT_BEFORE_SLEEP = 2500;
+  static constexpr uint16_t CHANGE_COUNT_BEFORE_SLEEP = 1000;
 
   static volatile bool interruptsEnabled;
   static volatile bool interruptTriggered;
