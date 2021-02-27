8fdf6ae11fc3e0aa6a3570bb75b0442a8494747b
Kevin Frei <kevinfrei@hotmail.com>
2021-02-27 11:51:31 -0800

    enum class instead of constexpr array of ints

--- src/include/comm.h
+++ src/include/comm.h
@@ -15,26 +15,60 @@ constexpr uint8_t WHICH_SIDE = LEFT_SIDE;
 #elif defined(RIGHT)
 constexpr uint8_t WHICH_SIDE = RIGHT_SIDE;
 #endif
-namespace types {
-#if defined(DEBUG) && DEBUG > 1
-constexpr char* COMM_TYPE_NAME[] =
-    { "Scan", "Battery", "Time", "Sync", "SetLed", "SetRed", "SetBlue", "ERROR!" };
-#endif
-constexpr uint8_t SCAN = 0;
-constexpr uint8_t BATTERY = 1;
-constexpr uint8_t TIME = 2;
-constexpr uint8_t SYNC = 3;
-constexpr uint8_t SETLED = 4;
-constexpr uint8_t SETRED = 5;
-constexpr uint8_t SETBLUE = 6;
-constexpr uint8_t count = 7;
-} // namespace types
 
-constexpr std::array<uint8_t, types::count> sizes{
-    MatrixBits::num_bytes, 1, sizeof(uint32_t), 0, 1, 1, 1};
+// Message types (only 3 bits, currently)
+enum class types : uint8_t {
+  SCAN = 0,
+  BATTERY = 1,
+  TIME = 2,
+  SYNC = 3,
+  SETLED = 4,
+  SETRED = 5,
+  SETBLUE = 6,
+};
+
+// Get the packet size of the different messages above
+constexpr uint8_t types_size(types t) {
+  switch (t) {
+    case types::SCAN:
+      return MatrixBits::num_bytes;
+    case types::BATTERY:
+    case types::SETLED:
+    case types::SETRED:
+    case types::SETBLUE:
+      return 1;
+    case types::TIME:
+      return sizeof(uint32_t);
+    case types::SYNC:
+      return 0;
+  };
+}
+
+#if defined(DEBUG) && DEBUG > 1
+// String name for diagnostics...
+constexpr char* get_type_name(types t) {
+  switch (t) {
+    case types::SCAN:
+      return "Scan";
+    case types::BATTERY:
+      return "Battery";
+    case types::SETLED:
+      return "SetLED";
+    case types::SETRED:
+      return "SetRed";
+    case types::SETBLUE:
+      return "SetBlue";
+    case types::TIME:
+      return "Time";
+    case types::SYNC:
+      return "Sync";
+  }
+}
+#endif
+
 struct header {
   uint8_t side : 1;
-  uint8_t type : 3;
+  types type : 3;
   uint8_t size : 4;
 };
 
@@ -49,13 +83,13 @@ uint8_t getSide(UART& uart) {
 #endif
 }
 
-template <uint8_t VAL, typename T, typename UART>
+template <comm::types VAL, typename T, typename UART>
 void send_packet(UART& uart, const T& v) {
   comm::header h;
   char buffer[sizeof(h) + sizeof(T)];
   h.side = getSide(uart);
   h.type = VAL;
-  h.size = comm::sizes[VAL];
+  h.size = comm::types_size(VAL);
   memcpy(&buffer[0], reinterpret_cast<char*>(&h), sizeof(h));
   memcpy(&buffer[sizeof(h)], reinterpret_cast<const char*>(&v), sizeof(T));
 #if defined(DEBUG) && DEBUG > 1
@@ -67,12 +101,13 @@ void send_packet(UART& uart, const T& v) {
   uart.write(buffer, sizeof(buffer));
 };
 
-template <uint8_t VAL, typename UART>
+template <comm::types VAL, typename UART>
 void send_packet(UART& uart) {
   comm::header h;
   h.side = getSide(uart);
   h.type = VAL;
-  h.size = comm::sizes[VAL];
+  static_assert(comm::types_size(VAL) == 0);
+  h.size = 0;
   DBG2(dumpHex((uint8_t)(*(reinterpret_cast<char*>(&h))), "SB:"));
   uart.write(*reinterpret_cast<char*>(&h));
 };
