d8c89d9ac23c5b8f011b9f37ef6a627a9e8bc8eb
Kevin Frei <kevinfrei@hotmail.com>
2022-01-05 16:47:02 -0800

    Updating to latest...Slowly

--- src/make-af_nrf52.mk.sh
+++ src/make-af_nrf52.mk.sh
@@ -1,1 +1,1 @@
-./libs/Arduino2Make/ar2mak.js libs/Adafruit libs/BusIO libs/GFX libs/NeoPixel libs/SSD1306 libs/TinyUSB > af_nrf52.mk
+./libs/Arduino2Make/ar2mak.js libs/Adafruit libs/BusIO libs/GFX libs/NeoPixel libs/SSD1306 libs/TinyUSB libs/nRFCrypto > af_nrf52.mk
