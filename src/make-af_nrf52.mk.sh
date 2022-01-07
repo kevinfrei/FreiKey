330a6e2c6c1b5a7849c2a70e29d5f20f4ae4912b
Kevin Frei <kevinfrei@hotmail.com>
2022-01-07 15:40:51 -0800

    Updated Arduino2Make and the config script to work with latest Adafruit stuff

--- src/make-af_nrf52.mk.sh
+++ src/make-af_nrf52.mk.sh
@@ -1,1 +1,1 @@
-./libs/Arduino2Make/ar2mak.js libs/Adafruit libs/BusIO libs/GFX libs/NeoPixel libs/SSD1306 libs/TinyUSB libs/nRFCrypto > af_nrf52.mk
+./libs/Arduino2Make/ar2mak.js --config:adafruit-make-config.json libs/Adafruit libs/BusIO libs/GFX libs/NeoPixel libs/SSD1306 libs/TinyUSB libs/nRFCrypto > af_nrf52.mk
