bfb333692dd687312b94ca40bc522bfc62b7e40c
Kevin Frei <kevinfrei@hotmail.com>
2022-04-03 22:11:45 -0700

    Starting the parser

--- src/CalcParsing.cpp
+++ src/CalcParsing.cpp
@@ -1,5 +1,7 @@
 #include <cctype>
+#include <functional>
 #include <iostream>
+#include <map>
 #include <stdint.h>
 #include <string>
 #include <vector>
@@ -208,6 +210,148 @@ std::vector<Token> Tokenize(const std::string& str) {
   return res;
 }
 
+class CalcValue {
+  bool isFlt;
+  double fVal;
+  int64_t iVal;
+  const char* err;
+
+ public:
+  CalcValue(double d) : isFlt(true), fVal(d), iVal(0), err(nullptr) {}
+  CalcValue(int64_t i) : isFlt(false), fVal(0.0), iVal(i), err(nullptr) {}
+  CalcValue(int i) : isFlt(false), fVal(0.0), iVal(i), err(nullptr) {}
+  CalcValue(uint64_t i) : isFlt(false), fVal(0.0), iVal(i), err(nullptr) {}
+  CalcValue(int8_t i) : isFlt(false), fVal(0.0), iVal(i), err(nullptr) {}
+  CalcValue(uint8_t i) : isFlt(false), fVal(0.0), iVal(i), err(nullptr) {}
+  CalcValue(const char* e = nullptr)
+    : isFlt(true), fVal(0), iVal(-1LL), err(e) {}
+  bool isError() const {
+    return isFloat && (iVal == -1LL || err != nullptr);
+  }
+  double asDouble() const {
+    return isFloat ? fVal : static_cast<double>(iVal);
+  }
+  int64_t asInt() const {
+    return isFloat ? static_cast<double>(fVal) : iVal;
+  }
+  bool isFloat() const {
+    return isFloat;
+  }
+};
+
+using CalcFunction = std::function<CalcValue(CalcValue)>;
+
+/*
+ * A little recursive-descent parser
+ *
+ * The Grammar it all it's EBNF-ish glory:
+ *
+ * line := expr EOF | assign EOF
+ * expr := prod | expr ADD prod | expr SUB prod
+ * prod := val | val MUL prod | val DIV prod | val MOD prod
+ * numish := NUM | func | VAR | OPAREN expr CPAREN | MINUS numish | numish EXP
+ * numish | numish FACT func := VAR OPAREN expr CPAREN assign := VAR EQU expr
+ *
+ */
+class CalculatorState {
+  // Do something goofy with pi, i, and e, probably.
+  // Everything is lowercase, cuz...
+  std::map<std::string, CalcValue> vars;
+  std::map<std::string, CalcFunction> funcs;
+
+  std::vector<Token> tokens;
+  size_t cur;
+
+  Token& cur() const {
+    return tokens[cur];
+  }
+
+  Token& peek() const {
+    return tokens[cur + 1];
+  }
+
+  void next() {
+    cur++;
+  }
+
+  void back() {
+    cur--;
+  }
+
+  CalcValue Expr() {
+    CalcValue v;
+    return v;
+  }
+
+  CalcValue Assign(size_t& cur) {
+    CalcValue v;
+    return v;
+  }
+
+  CalcValue Prod(size_t& cur) {
+    CalcValue v;
+    return v;
+  }
+
+  CalcValue Numish(size_t& cur) {
+    CalcValue v;
+    return v;
+  }
+
+  CalcValue Func(size_t& cur) {
+    CalcValue v;
+    return v;
+  }
+
+  CalcValue Line() {
+    CalcValue v;
+    cur = 0;
+    if (tokens[end].tok == TokenID::Eof) {
+      if (end == 0) {
+        v = CalcValue{"Empty!"};
+      }
+      k
+    }
+    return v;
+  }
+
+ public:
+  // Maybe, when CalcValue isn't just an int or double, set FP precision for
+  // transcendentals?
+  CalculatorState() {
+    // Eventually in C++20: std::numbers::pi
+    vars["pi"] = CalcValue{3.14159265358979323846};
+    // Eventually, std::numbers::e
+    vars["e"] = CalcValue{2.71828182845904523536};
+    // Nothing in functions just yet...
+  }
+  CalcValue Parse(const std::string& str) {
+    tokens = Tokenize(str);
+    CalcValue cv = Entry();
+    return cv;
+  }
+};
+
+int main(int argc, const char* argv[]) {
+  std::string input;
+  CalculatorState state;
+  do {
+    std::cout << "Enter some stuff:" << std::endl;
+    std::getline(std::cin, input);
+    std::cout << "'" << input << "'" << std::endl;
+    CalcValue cv = state.Parse(input);
+    if (cv.isError()) {
+      std::cout << "Error!" << std::endl;
+    } else if (cv.isFloat) {
+      std::cout << cv.fVal << std::endl;
+    } else {
+      std::cout << cv.iVal << std::endl;
+    }
+    std::cout << "====" << std::endl;
+  } while (!input.empty());
+  return 0;
+}
+
 #if defined(TEST_TOKENIZATION)
 void Calculate(const std::string& str) {
   for (auto& token : Tokenize(str)) {
@@ -277,3 +421,11 @@ int main(int argc, const char* argv[]) {
   return 0;
 }
 #endif
+
+/*
+ * Silly stuff for much later:
+ * The Nilakantha series converges on pi quicker than Leibniz:
+ * π = 3 + 4/(2*3*4) - 4/(4*5*6) + 4/(6*7*8) - 4/(8*9*10) + 4/(10*11*12)...
+ * Slightly simplified:
+ * π = 3 + 1/(1*3*2) - 1/(2*5*3) + 1/(3*7*4) - 1/(4*9*5) + 1/(5*11*6)...
+ */
