acb95fa2f20b860f03a6f8f43d238fab7d6c5085
Kevin Frei <kevinfrei@hotmail.com>
2022-05-10 09:16:45 -0700

    Initial stuff for my Arduino-flavored std::filesystem stuff

---
+++ src/ardfs.cpp
@@ -0,0 +1,35 @@
+#include "ardfs.h"
+
+namespace ard {
+namespace filesystem {
+
+path cur("/");
+
+path current_path() {
+  return cur;
+}
+
+void current_path(const path& p) {
+  cur = p;
+}
+
+bool remove(const ard::filesystem::path& p) {
+  // TODO
+  return false;
+}
+
+bool remove(const ard::filesystem::path& p, std::error_code& ec) noexcept {
+  // TODO
+  return false;
+}
+
+std::uintmax_t remove_all(const ard::filesystem::path& p) {
+  return 0;
+}
+
+std::uintmax_t remove_all(const ard::filesystem::path& p, std::error_code& ec) {
+  return 0;
+}
+
+} // namespace filesystem
+} // namespace ard
