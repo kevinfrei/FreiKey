b1dccf72b136e3a93e6db17521772af94ef5db45
Kevin Frei <kevinfrei@hotmail.com>
2022-09-07 07:19:26 -0700

    Fixed minor build issue with Makefiles. CMake here we come\?

--- src/modules/calculator/include.mk
+++ src/modules/calculator/include.mk
@@ -5,12 +5,12 @@ CALC_PARSER_FILES=${CALC_MODULE_DIR}CalcParser.cpp ${CALC_MODULE_DIR}include/Cal
 USER_CLEAN = ${USER_CLEAN} ${CALC_PARSER_FILES}
 
 ${CALC_PARSER_FILES}: ${CALC_MODULE_DIR}CalcGrammar.yy
-	cd ${CALC_MODULE_DIR} && ${BISON} CalcGrammar.yy
+	cd ${CALC_MODULE_DIR} && ${BISON} --header=include/CalcParser.h --output=CalcParser.cpp CalcGrammar.yy
 
 # one minor dependency...
 ${CALC_MODULE_DIR}CalcLexer.cpp : ${CALC_MODULE_DIR}include/CalcParser.h
 
-USER_INCLUDES += -Imodules/calculator -Imodules/calculator/include
+USER_INCLUDES += -Imodules/calculator -Imodules/calculator/include -Imodules/calculator/headers
 USER_CPP_SRCS += $(addprefix ${CALC_MODULE_DIR}, CalcLexer.cpp CalcParser.cpp Calculator.cpp CalcExpr.cpp CalcContext.cpp CalcHandler.cpp)
 
 VPATH += ${CALC_MODULE_DIR}
