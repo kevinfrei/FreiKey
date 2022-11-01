4e016506e15d0aa2a8a77fff55aaa214f20f9fea
Kevin Frei <kevinfrei@hotmail.com>
2022-11-01 08:48:09 -0700

    Something about Keyboard LEDs for Teensy - untested

--- src/include/teensy/mpu.h
+++ src/include/teensy/mpu.h
@@ -2,6 +2,8 @@
 
 #include "sysstuff.h"
 
+// Bit 1/1 == num lock, Bit 2/2 == caps lock, Bit 3/4 == scroll lock
+extern volatile uint8_t keyboard_leds;
 struct Teensy {
   // This configuration make sit so the Teensy LED (on pin 13)
   // doesn't stay lit 99.999% of the time...
@@ -18,5 +20,14 @@ struct Teensy {
   }
   static void completePin(uint8_t pin) {
     pinMode(pin, INPUT);
+  }
+  static bool isNumLocked() {
+    return keyboard_leds & 1;
+  }
+  static bool isCapsLocked() {
+    return keyboard_leds & 2;
+  }
+  static bool isScrollLocked() {
+    return keyboard_leds & 4;
   }
 };
