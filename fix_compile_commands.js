d1bd3659de95e3b80f4c3dd0d7aa26927af345a7
Kevin Frei <kevinfrei@hotmail.com>
2023-03-22 22:03:42 -0700

    Got the compile_commands stuff working in a different manner

--- fix_compile_commands.js
+++ fix_compile_commands.js
@@ -7,7 +7,7 @@ const path = require('path');
 const fsp = require('fs').promises;
 
 const rootLoc = path.join('src', 'out');
-const ccname = 'compile_commands.json';
+const ccname = 'compile_commands.jsn';
 // File all the compile_commands.json files underneath src/out
 async function findFiles() {
   const files = await fsp.readdir(rootLoc);
@@ -23,13 +23,36 @@ async function findFiles() {
 }
 
 async function fixFile(filename) {
-  const text = await fsp.readFile(filename);
-  const contents = JSON.parse(text);
-  const fixed = contents
-    .filter((v) => v.directory !== undefined)
-    .map((vn) => (vn.directory === '.' ? { ...vn, directory: 'src' } : vn));
-  const jsonCC = JSON.stringify(fixed, null, ' ');
-  await fsp.writeFile(filename, jsonCC);
+  const text = (await fsp.readFile(filename))
+    .toString()
+    .split('\n')
+    .map((v) => v.trim());
+  const contents = [];
+  for (let i = 0; i < text.length - 2; i += 3) {
+    const obj = {};
+    const ln1 = text[i];
+    let flag = 0;
+    if (ln1.startsWith('dir#')) {
+      const dir = ln1.substring(4);
+      obj.directory = dir === '.' ? 'src' : dir;
+    }
+    flag += 1;
+    const ln2 = text[i + 1];
+    if (ln2.startsWith('fil#')) {
+      obj.file = ln2.substring(4);
+      flag += 2;
+    }
+    const ln3 = text[i + 2];
+    if (ln3.startsWith('cmd#')) {
+      obj.command = ln3.substring(4);
+      flag += 4;
+    }
+    if (flag === 7) {
+      contents.push(obj);
+    }
+  }
+  const jsonCC = JSON.stringify(contents, null, ' ');
+  await fsp.writeFile(filename.replace('.jsn', '.json'), jsonCC);
 }
 
 async function doit() {
